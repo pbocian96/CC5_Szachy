ac9f27f14c8e731311a73a24f4ffb93c7da0eee4
Arkadiusz Klonowski <arkadiusz.klonowski.1995@gmail.com>
2019-11-06 21:25:46 +0100

    adding knight implementation adding knights in correct places on board

--- src/board.js
+++ src/board.js
@@ -1,5 +1,9 @@
 import Pawn from './pieces/pawn';
-
+import Knight from './pieces/knight';
+import Bishop from './pieces/bishop';
+import King from './pieces/king';
+import Queen from './pieces/queen';
+import Rook from './pieces/rook';
 const board = new Array(8);
 for (let i = 0; i < 8; i++) {
     board[i] = new Array(8);
@@ -9,5 +13,13 @@ let pawn = new Pawn(6, 0, 'white');
 board[pawn.x][pawn.y] = pawn;
 pawn = new Pawn(6, 1, 'white');
 board[pawn.x][pawn.y] = pawn;
+let knight = new Knight(7, 1, 'white');
+board[knight.x][knight.y] = knight;
+knight = new Knight(7, 6, 'white');
+board[knight.x][knight.y] = knight;
+knight = new Knight(0, 1, 'black');
+board[knight.x][knight.y] = knight;
+knight = new Knight(0, 6, 'black');
+board[knight.x][knight.y] = knight;
 
 export default board;
--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -9,6 +9,15 @@ class Knight extends Piece {
     findLegalMoves() {
 
         const possibleMoves = [];
+        for (let i = 1; i <= 2; i++) {
+            for (let xSign = -1; xSign <= 1; xSign+=2) {
+                for (let ySign = -1; ySign <= 1; ySign+=2) {
+                    let xVector = i*xSign;
+                    let yVector = (3-i)*ySign;
+                    this.x + xVector >= 0 && this.x + xVector <= 7 && this.y + yVector >= 0 && this.y + yVector <=7 && possibleMoves.push(`${this.x + xVector},${this.y + yVector}`);
+                }
+            }
+        }
 
         return possibleMoves;
     }
